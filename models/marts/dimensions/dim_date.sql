dea8fd80429a5e03b27be1d3c5b708bb3a539ef7
Saloni Prabhu <prabhusa@usc.edu>
2025-07-23 16:30:43 -0700

    Update change in dim_date.sql

--- models/marts/dimensions/dim_date.sql
+++ models/marts/dimensions/dim_date.sql
@@ -1,21 +1,25 @@
 {{ config(
     materialized='table',
     schema='marts'
-)}}
+) }}
 
-WITH dates AS(
-    SELECT date AS calendar_date
-    FROM unnest(generate_date_array('2024-01-01','2025-12-31', interval 1 day)) AS date
+WITH date_range AS (
+    SELECT
+        DATEADD(DAY, seq4(), DATE '2024-01-01') AS calendar_date
+    FROM TABLE(GENERATOR(ROWCOUNT => 731)) -- 731 = number of days from Jan 1, 2024 to Dec 31, 2025
 )
 
-SELECT 
+SELECT
     calendar_date,
     EXTRACT(YEAR FROM calendar_date) AS year,
     EXTRACT(QUARTER FROM calendar_date) AS quarter,
     EXTRACT(MONTH FROM calendar_date) AS month,
-    FORMAT_DATE('%B', calendar_date) AS month_name,
-    FORMAT_DATE('%A', calendar_date) AS day_name,
+    TO_CHAR(calendar_date, 'Month') AS month_name,
+    TO_CHAR(calendar_date, 'Day') AS day_name,
     EXTRACT(DAYOFWEEK FROM calendar_date) AS day_of_week,
     EXTRACT(WEEK FROM calendar_date) AS week,
-    CASE WHEN EXTRACT(DAYOFWEEK FROM calendar_date) in (1,7) then 'Weekend' ELSE 'Weekday' END AS is_weekend
-FROM dates
+    CASE 
+        WHEN EXTRACT(DAYOFWEEK FROM calendar_date) IN (1, 7) THEN 'Weekend'
+        ELSE 'Weekday'
+    END AS is_weekend
+FROM date_range
